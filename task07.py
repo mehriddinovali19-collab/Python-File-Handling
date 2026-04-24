4d1f4a25b1861bda587c0c054e46978011250001
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-24 13:10:41 +0500

    task07 done

--- task07.py
+++ task07.py
@@ -3,10 +3,12 @@ data = input_file.read()
 
 kv = data.split()
 numbers = [int(x) for x in kv]
+
+output_file = open("Output/output07.txt", "w") 
+
 for num in numbers:
     result = pow(num, 2)
-    output_file = open("Output/output07.txt", "w")
-    output_file.write(str(result))
+    output_file.write(str(result) + "\n") 
 
 
     
