9fff5b0b714bdac8f757ec425f48106c962821fc
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-24 13:31:27 +0500

    task08 done

---
+++ task08.py
@@ -0,0 +1,14 @@
+input_file = open("Input/numbers.txt", "r")
+data = input_file.read()
+
+five = data.split()
+numbers =[int(x) for x in five]
+
+five_multi = [num for num in numbers if num % 5 == 0]
+count = len(five_multi)
+
+Output_file = open("Output/output08.txt", "w")
+Output_file.write(f"Sonlar: {five_multi}\nSoni: {count}")
+
+
+
